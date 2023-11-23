e3a221b22dff058d775b9a29949fbe32ff0a8ba9
Noman <naumanliaqat4546@gmail.com>
2023-11-23 14:05:44 +0500

    intial commit

---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+from. import counter
+def home(request):
+    return render(request, 'index.html', {'key1':'This is coming from python code'} )
+def result(request):
+    article = request.GET['article']
+    var_dict, word_count =counter.count(article)
+    return render(request, 'result.html', {'article': article, 'word_count': word_count, 'dict_words': var_dict} )
